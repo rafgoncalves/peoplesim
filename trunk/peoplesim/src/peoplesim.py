92836bd46b68ae6a021ffaec8d61c5908c5bfa83
rafael.augusto <rafael.augusto@6ab428e9-6026-a0bb-57f8-1540e0c8cd3e>
2010-09-29 02:17:42 +0000

    Testing

---
+++ trunk/peoplesim/src/peoplesim.py
@@ -0,0 +1,9 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Rafael"
+__date__ ="$Sep 28, 2010 11:14:45 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
+
